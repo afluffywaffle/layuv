a7329e3b98f5c7eb40b01a868d384acfde7424a8
Jayrom Acorda <jayrom.acorda@icloud.com>
2026-06-01 22:28:27 -0700

    Clean build confirmed on Android, macOS, and iOS

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 android {
     namespace = "com.afluffywaffle.layuv"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
